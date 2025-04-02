1251b1bd477e99ce3ba2a8d16879077ba56391c3
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-04-02 11:07:55 +0200

    Add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,10 @@
+const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    if (randomNumber === 1) {
+        return "rock";
+    } else if (randomNumber === 2) {
+        return "paper";
+    } else  {
+        return "scissors";
+    }
+}
